d6e78958dd1bc7ab2d8ea48df604bc7116e4493d
Enos <enoshenrique008@gmail.com>
2023-05-08 14:41:23 -0300

    implemented king's possibles moves

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -86,7 +86,7 @@ public class ChessMatch {
 		placeNewPiece('b' , 2 ,new Pawn(board, Color.WHITE));
 		placeNewPiece('f' , 2 ,new Pawn(board, Color.WHITE));
 		placeNewPiece('c' , 2 ,new Pawn(board, Color.WHITE));
-		placeNewPiece('e' , 2 ,new Pawn(board, Color.WHITE));
+		placeNewPiece('e' , 2 ,new Pawn(board, Color.BLACK));
 		placeNewPiece('d' , 2 ,new Pawn(board, Color.WHITE));
 		
 		placeNewPiece('a' , 8 ,new Rook(board, Color.BLACK));
